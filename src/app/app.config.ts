f8e0068481cb098b96159eaeea29998420a847d8
DennisBilowodskyj <d.bilowodskyi@gmail.com>
2024-03-07 12:40:46 +0100

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { initializeApp } from "firebase/app";
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -7,3 +8,14 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync()]
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXB5-u3U67sg7xAugl-92ha5yiZ5_844E",
+  authDomain: "ring-of-fire-7f7a7.firebaseapp.com",
+  projectId: "ring-of-fire-7f7a7",
+  storageBucket: "ring-of-fire-7f7a7.appspot.com",
+  messagingSenderId: "747622262659",
+  appId: "1:747622262659:web:475bfbc8f9f284b23eb8b6"
+};
+
+const app = initializeApp(firebaseConfig);
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,68 +4,92 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialog,} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import  {MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { GameInfoComponent } from '../game-info/game-info.component';
+import { inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, DialogAddPlayerComponent,MatFormFieldModule,MatInputModule,FormsModule,MatDialogModule, GameInfoComponent ],
+  imports: [
+    CommonModule,
+    PlayerComponent,
+    MatButtonModule,
+    MatIconModule,
+    DialogAddPlayerComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatDialogModule,
+    GameInfoComponent,
+    
+  ],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  currentCard: string | undefined = "";  
+  currentCard: string | undefined = '';
   game: Game;
+  firestore: Firestore = inject(Firestore);
+  games$: Observable<any[]>;
 
-  constructor(public dialog: MatDialog){
+  constructor(public dialog: MatDialog) {
     this.game = new Game();
-    
+    const aCollection = collection(this.firestore, 'games');
+    this.games$ = collectionData(aCollection);
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.newGame();
+    this.games$.subscribe((games) =>{
+      console.log('game update', games)
+    });
+      
+     
+      
+     
   }
 
-  takeCard(){
-    if(!this.pickCardAnimation){
-    this.currentCard = this.game.stack.pop();
-    this.pickCardAnimation = true;
-    console.log('New card: ' + this.currentCard);
-    console.log('Game is: ' + this.game);
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log('New card: ' + this.currentCard);
+      console.log('Game is: ' + this.game);
 
-    this.game.currentPlayer ++;
-    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      this.game.currentPlayer++;
+      this.game.currentPlayer =
+        this.game.currentPlayer % this.game.players.length;
 
-
-    setTimeout(() =>{
-      if (this.currentCard != undefined) {
-        this.game.playedCards.push(this.currentCard);
-        this.pickCardAnimation = false;
-      }
-    }, 1000);
+      setTimeout(() => {
+        if (this.currentCard != undefined) {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }
+      }, 1000);
+    }
   }
-  }
 
-  newGame(){
+  newGame() {
     this.game = new Game();
     console.log(this.game);
   }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
-    });
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {});
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      if(name && name.length > 0){
-    this.game.players.push(name);   
-      } 
-});
+      if (name && name.length > 0) {
+        this.game.players.push(name);
+      }
+    });
   }
 }
